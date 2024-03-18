1ed61634035ba180b559839337e26a20ae616fd3
kang.song <songkang1993@outlook.com>
2024-03-18 18:13:36 +0800

    attach

--- web/src/utils/date.js
+++ web/src/utils/date.js
@@ -22,4 +22,21 @@ function getMonthFirstAndLastDay(date) {
     return [formatDate(firstDay), formatDate(lastDay)];
 }
 
-export { getMonthFirstAndLastDay}
+function formatDate(date) {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+}
+
+function formatTime(date) {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const second = date.getSeconds();
+    return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')} ${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:${second.toString().padStart(2, '0')}`;
+}
+
+export {getMonthFirstAndLastDay, formatDate, formatTime}
